fb66fdf6ca8d8cf13c47160c94b2a33934e31d0c
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2014-01-24 12:03:20 +0900

    refactoring

--- lib/Redis/Setlock.pm
+++ lib/Redis/Setlock.pm
@@ -29,7 +29,7 @@ END_OF_SCRIPT
 ;
 
 sub parse_options {
-    my ($class, @argv) = @_;
+    my (@argv) = @_;
 
     my $p = Getopt::Long::Parser->new(
         config => [qw/posix_default no_ignore_case auto_help bundling pass_through/]
@@ -47,7 +47,7 @@ sub parse_options {
         x
         X
     /) or pod2usage;
-    $opt->{wait}      = 0 if $opt->{n};  # no lock
+    $opt->{wait}      = 0 if $opt->{n};  # no delay
     $opt->{exit_code} = 0 if $opt->{x};  # exit code 0
     $opt->{expires}   = $DEFAULT_EXPIRES unless defined $opt->{expires};
 
@@ -57,18 +57,37 @@ sub parse_options {
 sub run {
     my $class = shift;
 
-    local $Log::Minimal::PRINT = sub {
-        my ( $time, $type, $message, $trace) = @_;
-        warn "$time $$ $type $message\n";
-    };
+    local $Log::Minimal::PRINT = \&log_minimal_print;
 
-    my ($opt, $key, @argv) = $class->parse_options(@_);
+    my ($opt, $key, @command) = parse_options(@_);
 
-    pod2usage() if !defined $key || @argv == 0;
+    pod2usage() if !defined $key || @command == 0;
 
-    my $redis;
+    my $redis = connect_to_redis_server($opt)
+        or return EXIT_CODE_REDIS_DEAD;
+
+    validate_redis_version($redis)
+        or return EXIT_CODE_REDIS_UNSUPPORTED_VERSION;
+
+    if ( my $token = try_get_lock($redis, $opt, $key) ) {
+        my $code = invoke_command(@command);
+        release_lock($redis, $opt, $key, $token);
+        return $code;
+    }
+    else {
+        # couldnot get lock
+        if ($opt->{exit_code}) {
+            critf "unable to lock %s.", $key;
+            return $opt->{exit_code};
+        }
+        return 0; # by option x
+    }
+}
+
+sub connect_to_redis_server {
+    my $opt = shift;
     try {
-        $redis = Redis->new(
+        Redis->new(
             server    => $opt->{redis},
             reconnect => $opt->{wait} ? $opt->{expires} : 0,
         );
@@ -78,8 +97,11 @@ sub run {
         my $error = (split(/\n/, $e))[0];
         critf "Redis server seems down: %s", $error;
         return;
-    } or return EXIT_CODE_REDIS_DEAD;
+    };
+}
 
+sub validate_redis_version {
+    my $redis = shift;
     my $version = $redis->info->{redis_version};
     debugf "Redis version is: %s", $version;
     my ($major, $minor, $rev) = split /\./, $version;
@@ -88,57 +110,64 @@ sub run {
       || $major == 2 && $minor == 6 && $rev >= 12
     ) {
         # ok
+        return 1;
     }
-    else {
-        critf "required Redis server version >= 2.6.12. current server version is %s", $version;
-        return EXIT_CODE_REDIS_UNSUPPORTED_VERSION;
-    }
+    critf "required Redis server version >= 2.6.12. current server version is %s", $version;
+    return;
+}
 
-    my $expires = $opt->{expires};
-    my $locked;
-    my $token = _token();
+sub try_get_lock {
+    my ($redis, $opt, $key) = @_;
+    my $got_lock;
+    my $token = create_token();
+ GET_LOCK:
     while (1) {
-        my @command = ($key, $token, "EX", $expires, "NX");
-        debugf "redis: set @command";
-        my $r = $redis->set(@command);
-        if (defined $r) {
-            $locked = 1;
-            debugf "locked: %s", $key;
-            last;
+        my @args = ($key, $token, "EX", $opt->{expires}, "NX");
+        debugf "redis: SET @args";
+        $got_lock = $redis->set(@args);
+        if ($got_lock) {
+            debugf "got lock: %s", $key;
+            last GET_LOCK;
         }
-        if (!$opt->{wait}) { # no wait by option n
-            debugf "No wait mode. exit";
-            last;
-        }
-        my $sleep = rand();
-        debugf "unable to lock. retry after %f sec.", $sleep;
-        sleep $sleep;
-    }
-    if ($locked) {
-        debugf "invoking command: @argv";
-        my $code = system @argv;
-        $code = $code >> 8;       # to raw exit code
-        debugf "child exit with code: %s", $code;
-        if ($opt->{keep}) {
-            debugf "Keep lock key %s", $key;
+        elsif (!$opt->{wait}) { # no delay by option n
+            debugf "no delay mode. exit";
+            last GET_LOCK;
         }
         else {
-            debugf "Release lock key %s", $key;
-            $redis->eval(UNLOCK_LUA_SCRIPT, 1, $key, $token);
+            my $sleep = rand();
+            debugf "unable to lock. retry after %f sec.", $sleep;
+            sleep $sleep;
         }
-        return $code;
+    }
+    return $token if $got_lock;
+}
+
+sub release_lock {
+    my ($redis, $opt, $key, $token) = @_;
+    if ($opt->{keep}) {
+        debugf "Keep lock key %s", $key;
     }
     else {
-        # can't get lock
-        if ($opt->{exit_code}) {
-            critf "unable to lock %s.", $key;
-            return $opt->{exit_code};
-        }
-        return 0; # by option x
+        debugf "Release lock key %s", $key;
+        $redis->eval(UNLOCK_LUA_SCRIPT, 1, $key, $token);
     }
 }
 
-sub _token {
+sub invoke_command {
+    my @command = @_;
+    debugf "invoking command: @command";
+    my $code = system @command;
+    $code = $code >> 8;       # to raw exit code
+    debugf "child exit with code: %s", $code;
+    return $code;
+}
+
+sub log_minimal_print {
+    my ( $time, $type, $message, $trace) = @_;
+    warn "$time $$ $type $message\n";
+}
+
+sub create_token {
     Time::HiRes::time() . rand();
 }
 
